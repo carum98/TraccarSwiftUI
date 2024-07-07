5857c96b380a2833163c4a4f61056ba0ea86a3e3
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-07-07 17:16:27 -0600

    Initial Commit

---
+++ TraccarSwiftUI/TraccarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraccarSwiftUIApp.swift
+//  TraccarSwiftUI
+//
+//  Created by Carlos Eduardo Umaña Acevedo on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TraccarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
